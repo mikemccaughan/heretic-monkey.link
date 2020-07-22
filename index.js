f4272f2451bca37632262322707a2ec8f7650057
Mike McCaughan <1719723+mikemccaughan@users.noreply.github.com>
2020-07-21 22:05:31 -0400

    Update all the things

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3030;
 app.use(express.static("./"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 app.listen(port, () => {
     console.log(`Listening on port ${port}: http://localhost:${port}/`);
 });
