576b92794e706442ab749dff0bc702f719bc5ffc
Mike McCaughan <github@mikemccaughan.com>
2020-08-12 14:59:22 -0400

    update all the things

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const appInsights = require("applicationinsights");
+const appInsights = require('applicationinsights');
 appInsights
-    .setup("InstrumentationKey=d7d2ae6a-6c2f-4fa0-ae80-600ac14d63d0")
-    .start();
+  .setup('InstrumentationKey=d7d2ae6a-6c2f-4fa0-ae80-600ac14d63d0')
+  .start();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("./"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.use(express.static('./'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on http://localhost:${process.env.PORT || 3000}/`);
+  console.log(`Listening on http://localhost:${process.env.PORT || 3000}/`);
 });
